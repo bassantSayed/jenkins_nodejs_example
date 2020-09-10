7288b451eb0c51a1204f0090fe06e4f7b2993c64
bassant.sayed1 <bassant.sayed1@vodafone.com>
2020-09-11 01:13:22 +0200

    adding redis

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,8 +1,25 @@
 const express = require('express')
+var redis = require('redis');
 const app = express()
 const port = 3000
+var output= 0
+var client = redis.createClient(6379, 'redishost.service_discovery', {no_ready_check: true});
+
+client.on('error', function (err) {
+    console.log('Error ' + err);
+}); 
+
+client.on('connect', function() {
+    console.log('Connected to Redis');
+});
+
+client.set("foo", "bar", redis.print);
+
+client.get("foo", function (err, reply) {
+    output = reply.toString();
+});
 app.get('/', (req, res) => {
-    res.send('Hello from dev!')
+    res.send('Hello World from dev!\n'+ 'redis output ' + output)
   })
   
   app.listen(port, () => {
